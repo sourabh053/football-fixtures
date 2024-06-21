5ee662007d70464983c93469ffc5b10743dcdea7
Sourabh Thakur <sourabhthakur48@gmail.com>
2024-06-21 19:33:01 +0530

    initial commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise');
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv").config();
 
-dotenv.config();
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+const urlDB = `mysql://root:AqhtMaTbcpIvFOdVdnEtYUIspnCdHWVH@roundhouse.proxy.rlwy.net:47354/railway`
+const db = mysql.createPool(urlDB);
 
 // db.connect((err) => {
 //   if (err) {
